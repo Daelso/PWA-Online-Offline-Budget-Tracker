55e3ea9e068c8c8d381c777761c8c1bd7b2b7664
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-11-04 17:35:00 -0400

    Server.Js update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const PORT = 3001;
 
@@ -15,15 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
