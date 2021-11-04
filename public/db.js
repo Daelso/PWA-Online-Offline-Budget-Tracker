d95128a896704bbe2b80dfe7bfd1f959bdf04dd6
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-11-04 17:20:52 -0400

    Takes stored data and posts it

--- public/db.js
+++ public/db.js
@@ -26,18 +26,67 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
-//this goes off if we attempt to write to the offline site.
+//this function fires off if network is offline
 function saveRecord(record) {
 
-    // uses a built in transaction function, with the params of a new transaction that has both read and write permissions
+    // runs a transaction function creating a new_transaction with readwrite permissions, saves to transaction const
     const transaction = indexDB.transaction(['new_transaction'], 'readwrite');
 
-    // pass this transaction data onto budgetObjectStore
+    // access the object store for `new_transaction`
     const  budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // places the data onto the scoped record
+    // adds to the scoped param
     budgetObjectStore.add(record);
 }; 
 
+function uploadData() {
 
+    // open a transaction on the db
+    const transaction = indexDB.transaction(['new_transaction'], 'readwrite');
+
+    // accesses the objectStore
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // transfers all data onto getAll from bdugetObjectStore
+    const getAll = budgetObjectStore.getAll();
+
+    //on success of getAll run this functionn
+    getAll.onsuccess = function() {
+
+        // if there was data saved, we make a POST call through our api transaction route in routes/api.js
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    // open again
+                    const transaction = indexDB.transaction(['new_transaction'], 'readwrite');
+
+                    // access the new_transaction data
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+                    // clear out the store when done to avoid duplicates
+                    budgetObjectStore.clear();
+
+                    alert("Don't worry, your data has been submitted!");
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+// when the app comes "online", call the function
+window.addEventListener('online', uploadData); 
 
